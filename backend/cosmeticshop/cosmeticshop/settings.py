68a05cfef3c0dead26ced4b24151e3a4e330a08a
DarylAdrien <Edaryl2705@gmail.com>
2025-03-31 21:33:07 +0530

    Added Dockerfile

--- backend/cosmeticshop/cosmeticshop/settings.py
+++ backend/cosmeticshop/cosmeticshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xx4s0kmhxp711ih5hjiz=_y*9#b!!*+g@ao2knu8d6fdr7lm@j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 MEDIA_URL = '/media/'
